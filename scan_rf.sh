1f488c011eeebafa5b5b81a12aebd2b699b49dfa
Svyatoslav Sidorov <svet.sidorov@gmail.com>
2021-03-08 23:22:31 +0000

    Collected and polished scripts and tables

---
+++ scan_rf.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 4
+#SBATCH --mem 8G
+#SBATCH --time 02:00:00
+
+# To reproduce the run of InterProScan, you would need to change the paths below
+
+# This FASTA was queried from UniProt using UniProt IDs of proteins 
+# whose peptides were captured by both SPACE and 
+# the released fraction of SPACEmap
+fasta="input/Released_fraction.txt_overlapped_with_SPACE.fa"
+
+interproscan-5.47-82.0/interproscan.sh \
+    --output-dir interproscan_matches \
+    --input ${fasta} \
+    --iprlookup \
+    --tempdir ../tmp
