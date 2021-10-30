7e21487a7c51807eff8801f20989a8b1d5f73a29
Svyatoslav Sidorov <svet.sidorov@gmail.com>
2021-10-30 17:07:31 +0100

    Init with new SPACEmap data

--- scripts/scan_clf.sh
+++ scripts/scan_clf.sh
@@ -6,12 +6,14 @@
 #SBATCH --time 02:00:00
 
 # This FASTA was queried from UniProt using UniProt IDs of proteins 
-# whose peptides were captured by the crosslinked fraction of SPACEmap
-# (See the Uniprot.ID column in input/Crosslinked_fraction_CLF_proteins.tsv)
-fasta="input/Crosslinked_fraction_CLF_proteins.fa"
+# captured by both SPACE and the crosslinked fraction of SPACEmap
+# (See the Uniprot.ID column in input/SPACEmap_crosslinked_fraction_overlap_with_SPACE.tsv)
+fasta="input/SPACEmap_crosslinked_fraction_overlap_with_SPACE_protein.fa"
+
+mkdir tmp
 
 interproscan-5.47-82.0/interproscan.sh \
-    --output-dir interproscan_matches_clf \
+    --output-dir output/interproscan_matches_clf \
     --input ${fasta} \
     --iprlookup \
     --tempdir tmp
